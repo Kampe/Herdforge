b5c5920f6455e88e7c1c32e156284ca9dc5c9469
Kampe <nickkampe@proton.me>
2026-08-14 15:16:04 -0500

    fix(resources): guard signed statfs field conversions against G115 overflow

--- pkg/resources/statfs_unix.go
+++ pkg/resources/statfs_unix.go
@@ -16,10 +16,26 @@ func (OSBackend) StatFS(path string) (Capacity, error) {
 	if err := unix.Statfs(path, &stat); err != nil {
 		return Capacity{}, err
 	}
-	if stat.Bsize == 0 {
+	if stat.Bsize <= 0 {
 		return Capacity{}, fmt.Errorf("invalid filesystem block size")
 	}
 	blockSize := uint64(stat.Bsize)
 	fsid := fmt.Sprintf("%d:%d", stat.Fsid.Val[0], stat.Fsid.Val[1])
-	return capacityFromStatfs(fsid, fmt.Sprintf("%d", stat.Type), stat.Blocks, stat.Bavail, blockSize, stat.Files, stat.Ffree)
+
+	toUint64 := func(v int64) uint64 {
+		if v < 0 {
+			return 0
+		}
+		return uint64(v)
+	}
+
+	return capacityFromStatfs(
+		fsid,
+		fmt.Sprintf("%d", stat.Type),
+		toUint64(int64(stat.Blocks)),
+		toUint64(int64(stat.Bavail)),
+		blockSize,
+		toUint64(int64(stat.Files)),
+		toUint64(int64(stat.Ffree)),
+	)
 }
