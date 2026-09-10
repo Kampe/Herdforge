9b2ff6e0bb5814cf88a15c07bfb272986e9c5270
Kampe <nickkampe@proton.me>
2026-09-10 14:52:25 -0500

    test: hermeticize verifier fixture process

--- pkg/daemon/main_test.go
+++ pkg/daemon/main_test.go
@@ -15,7 +15,18 @@ func TestMain(m *testing.M) {
 		fmt.Fprintf(os.Stderr, "isolate test heavy-phase slots: %v\n", err)
 		os.Exit(1)
 	}
+	previousHermetic, hadHermetic := os.LookupEnv("HERD_HERMETIC_CONTAINER")
+	if err := os.Setenv("HERD_HERMETIC_CONTAINER", "1"); err != nil {
+		fmt.Fprintf(os.Stderr, "set hermetic test boundary: %v\n", err)
+		restore()
+		os.Exit(1)
+	}
 	code := m.Run()
+	if hadHermetic {
+		_ = os.Setenv("HERD_HERMETIC_CONTAINER", previousHermetic)
+	} else {
+		_ = os.Unsetenv("HERD_HERMETIC_CONTAINER")
+	}
 	restore()
 	os.Exit(code)
 }
